da38a3f9eb77f509521a5085da2573a474bcb82b
root <root@DESKTOP-JR0T6JP.localdomain>
2021-05-11 21:49:24 -0300

    Código comentado

--- include/menu.h
+++ include/menu.h
@@ -8,10 +8,22 @@
 #include "jogador.h"
 #include "batalha.h"
 
+/**
+ * @brief Inicializa o menu principal
+ * @param arquivoPlacar caminho do arquivo de placares
+ * @param arquivoLog  caminho do arquivo log
+ */
 void menuPrincipal(char* arquivoPlacar, char* arquivoLog);
 
+/**
+ * @brief Imprime as opções do menu principal
+ */
 void imprimeMenuPrincipal();
 
+/**
+ * @brief Menu para que o jogador possa escolher os Pokémons iniciais
+ * @param jogador struct Jogador
+ */
 void menuIniciais(Jogador *jogador);
 
 #endif
